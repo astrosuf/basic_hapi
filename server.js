64b9c3214e30a0f9e4c69fc13f5fe39b341e7818
Sufiyaan Mitha <sufiyaan.mitha@skycity.co.nz>
2019-04-12 09:28:54 +1200

    implemented a basic server which returns hello world to the front-end, need to start working on building out API

---
+++ server.js
@@ -0,0 +1,28 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server({
+  host:'localhost',
+  port: 3000
+});
+
+//launch the server
+const launch = async () => {
+  try {
+    await server.start();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server running at ${server.info.uri}`);
+}
+
+launch()
+
+//building a basic route
+server.route ({
+  method: 'GET',
+  path:'/',
+  handler: (request, reply) => {
+    return "Hello World"
+  }
+})
